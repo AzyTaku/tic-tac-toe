bf18d8f679a5efe3d5294c84475ff494daf58d16
AzyTaku <azlanshaveenraban1@gmail.com>
2024-11-28 18:51:47 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React + Vite
+
+# UNDER CONSTRUCTIONS
+
+100% original idea of building a tic-tac-toe game in react
---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-700">
+        <h1 className="text-4xl font-bold text-black hover:text-white cursor-pointer mb-4">
+          Hello, Tailwind with Vite!
+        </h1>
+        <p className="text-white text-2xl">
+          Welcome to TIC TAC TOE
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
